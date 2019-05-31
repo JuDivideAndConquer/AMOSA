095f004ce6ae7283375d88504cfd361a1e74a894
Md Sahil <287mdsahil@gmail.com>
2019-05-31 17:29:20 +0530

    Added functions to the evaluate function
    more functions still to be added

--- evaluate.py
+++ evaluate.py
@@ -8,6 +8,26 @@ def evaluate(input,amosaParams):
     elif(amosaParams.c_problem == 'SCH2'):
         amosaParams.d_eval = SCH2(input)
         return
+    elif(amosaParams.c_problem == 'DTLZ1'):
+        amosaParams.d_eval = DTLZ1(input,amosaParams.i_no_offunc)
+        return
+    elif(amosaParams.c_problem == 'DTLZ2'):
+        amosaParams.d_eval = DTLZ2(input,amosaParams.i_no_offunc)
+        return
+    elif(amosaParams.c_problem == 'DTLZ3'):
+        amosaParams.d_eval = DTLZ3(input,amosaParams.i_no_offunc)
+        return
+    elif(amosaParams.c_problem == 'DTLZ4'):
+        amosaParams.d_eval = DTLZ4(input,amosaParams.i_no_offunc)
+        return
     else:
         print ('Invalid arguement for amosaParams.c_problem\nExiting.')
         exit()
+
+
+a = AMOSAType()
+a.i_no_offunc = 3
+a.c_problem = 'DTLZ4'
+input = [0,0,0,0,0,0]
+evaluate(input,a)
+print (a.d_eval)
