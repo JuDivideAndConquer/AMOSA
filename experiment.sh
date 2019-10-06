db28bbb9d988518caa9713c0b8d7461d1472fd7f
Md Sahil <287mdsahil@gmail.com>
2019-10-06 14:42:53 +0530

    convergence values stored in files

--- experiment.sh
+++ experiment.sh
@@ -1,6 +1,12 @@
-for i in {1..30}
-do
+file="./convergence$2.csv"
+if [ -f $file ]; then
+    rm $file
+fi
+
+for i in {1..30}; do
     echo -e "\niteration $i |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
     ./amosa_real.py "$1" "$2"
-    ./convergence.py "$3"
+    conv=$(./convergence.py "$3")
+    echo "convergence : $conv"
+    echo -e "$conv" >>$file
 done
