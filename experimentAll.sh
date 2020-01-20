636b21593d6f5ba1ca74289ab96795a2b9c5660c
Md Sahil <287mdsahil@gmail.com>
2020-01-21 00:51:27 +0530

    added script to take experimental readings

---
+++ experimentAll.sh
@@ -0,0 +1,29 @@
+./experiment.sh DTLZ1 3 true_pareto_fronts/DTLZ1\(3\).csv 30
+./experiment.sh DTLZ2 3 true_pareto_fronts/DTLZ2\(3\).csv 30
+./experiment.sh DTLZ3 3 true_pareto_fronts/DTLZ2\(3\).csv 30
+./experiment.sh DTLZ4 3 true_pareto_fronts/DTLZ2\(3\).csv 30
+
+
+./experiment.sh DTLZ1 5 true_pareto_fronts/DTLZ1\(5\).csv 30
+./experiment.sh DTLZ2 5 true_pareto_fronts/DTLZ2\(5\).csv 30
+./experiment.sh DTLZ3 5 true_pareto_fronts/DTLZ2\(5\).csv 30
+./experiment.sh DTLZ4 5 true_pareto_fronts/DTLZ2\(5\).csv 30
+
+
+./experiment.sh DTLZ1 8 true_pareto_fronts/DTLZ1\(8\).csv 30
+./experiment.sh DTLZ2 8 true_pareto_fronts/DTLZ2\(8\).csv 30
+./experiment.sh DTLZ3 8 true_pareto_fronts/DTLZ2\(8\).csv 30
+./experiment.sh DTLZ4 8 true_pareto_fronts/DTLZ2\(8\).csv 30
+
+
+./experiment.sh DTLZ1 10 true_pareto_fronts/DTLZ1\(10\).csv 15
+./experiment.sh DTLZ2 10 true_pareto_fronts/DTLZ2\(10\).csv 15
+./experiment.sh DTLZ3 10 true_pareto_fronts/DTLZ2\(10\).csv 15
+./experiment.sh DTLZ4 10 true_pareto_fronts/DTLZ2\(10\).csv 15
+
+
+./experiment.sh DTLZ1 15 true_pareto_fronts/DTLZ1\(15\).csv 15
+./experiment.sh DTLZ2 15 true_pareto_fronts/DTLZ2\(15\).csv 15
+./experiment.sh DTLZ3 15 true_pareto_fronts/DTLZ2\(15\).csv 15
+./experiment.sh DTLZ4 15 true_pareto_fronts/DTLZ2\(15\).csv 15
+
